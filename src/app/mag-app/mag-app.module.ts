39bddde206eb0428fac3cf6a161e177b62d6a3b2
Matthew Green <mgreen@aamc.org>
2018-08-09 16:39:24 -0400

    [MAG RFCTR] Continued Styling of Home Page

--- src/app/mag-app/mag-app.module.ts
+++ src/app/mag-app/mag-app.module.ts
@@ -1,19 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
-import { MagAppRoutingModule } from './mag-app-routing.module';
-import { MainComponent } from './main/main.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { CareerComponent } from './career/career.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { MagAppRoutingModule } from "./mag-app-routing.module";
+import { MainComponent } from "./main/main.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { CareerComponent } from "./career/career.component";
+import { ProjectsComponent } from "./projects/projects.component";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FlexLayoutModule,
-    MagAppRoutingModule
-  ],
-  declarations: [MainComponent, AboutMeComponent, CareerComponent, ProjectsComponent]
+  imports: [CommonModule, FlexLayoutModule, MagAppRoutingModule],
+  declarations: [
+    MainComponent,
+    AboutMeComponent,
+    CareerComponent,
+    ProjectsComponent
+  ]
 })
-export class MagAppModule { }
+export class MagAppModule {}
