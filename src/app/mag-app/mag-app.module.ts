e374a97bd3c330423f75d50f972d64cfa9880487
Matthew Green <mgreen@aamc.org>
2018-08-10 12:14:22 -0400

    [MAG RFCTR] Stable Home; Affix Navbar; Standardize Components Height

--- src/app/mag-app/mag-app.module.ts
+++ src/app/mag-app/mag-app.module.ts
@@ -4,17 +4,19 @@ import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { MagAppRoutingModule } from "./mag-app-routing.module";
 import { MainComponent } from "./main/main.component";
-import { AboutMeComponent } from "./about-me/about-me.component";
+import { HomeComponent } from "./home/home.component";
 import { CareerComponent } from "./career/career.component";
 import { ProjectsComponent } from "./projects/projects.component";
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   imports: [CommonModule, FlexLayoutModule, MagAppRoutingModule],
   declarations: [
     MainComponent,
-    AboutMeComponent,
+    HomeComponent,
     CareerComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    AboutMeComponent
   ]
 })
 export class MagAppModule {}
--- src/app/rpdr-fl/auth/components/register/register.component.ts
+++ src/app/rpdr-fl/auth/components/register/register.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { first } from "rxjs/operators";
 
-import { AlertService, UserService } from '../../../core/services/';
+import { AlertService, UserService } from "../../../core/services";
 
-@Component(
-  {
-    templateUrl: 'register.component.html'
-  }
-)
+@Component({
+  templateUrl: "register.component.html"
+})
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   loading = false;
@@ -19,19 +17,22 @@ export class RegisterComponent implements OnInit {
     private formBuilder: FormBuilder,
     private router: Router,
     private userService: UserService,
-    private alertService: AlertService) { }
+    private alertService: AlertService
+  ) {}
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      username: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
+      username: ["", Validators.required],
+      password: ["", [Validators.required, Validators.minLength(6)]]
     });
   }
 
   // convenience getter for easy access to form fields
-  get f() { return this.registerForm.controls; }
+  get f() {
+    return this.registerForm.controls;
+  }
 
   onSubmit() {
     this.submitted = true;
@@ -42,16 +43,18 @@ export class RegisterComponent implements OnInit {
     }
 
     this.loading = true;
-    this.userService.register(this.registerForm.value)
+    this.userService
+      .register(this.registerForm.value)
       .pipe(first())
       .subscribe(
         data => {
-          this.alertService.success('Registration successful', true);
-          this.router.navigate(['/login']);
+          this.alertService.success("Registration successful", true);
+          this.router.navigate(["/login"]);
         },
         error => {
           this.alertService.error(error);
           this.loading = false;
-        });
+        }
+      );
   }
 }
