ff659ea1af9079d248e4c6f607204027f1db76eb
Matt Green <verde.mateo.a@gmail.com>
2019-07-15 21:46:53 -0400

    [Content-Update] Create Work History Component

--- src/app/mag-app/components/content/career/career.component.ts
+++ src/app/mag-app/components/content/career/career.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Position } from "./index";
 
 @Component({
   selector: "mag-career",
@@ -6,6 +7,21 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./career.component.scss"]
 })
 export class CareerComponent implements OnInit {
+  jerbs: Position[] = [
+    {
+      title: "Software Engineer",
+      dates: "Jan 2018-Present",
+      blurb: "Lead Developer on the AMCAS For School Application",
+      company: "Association of American Medical College",
+      location: null,
+      tasks: [
+        "Led the implementation of an Angular Project using HighCharts and Angular Material",
+        "Added Google Analytics Code to two AAMC apps",
+        "Maintained and Added functionality to Backend Services"
+      ]
+    }
+  ];
+
   constructor() {}
 
   ngOnInit() {}
