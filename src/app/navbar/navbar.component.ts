9b830e52c920863b6fee24e7e83727a26ebac7ec
Matt Green <verde.mateo.a@gmail.com>
2020-03-07 16:19:32 -0500

    [Clean Up]: Adds Footer

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'mag-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
